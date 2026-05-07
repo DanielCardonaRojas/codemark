5f2840f13461194821c4b651341a772a205d80c9
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2026-05-06 23:02:16 -0500

    Bump schema version

--- crates/codetours-server/src/storage/manager.rs
+++ crates/codetours-server/src/storage/manager.rs
@@ -94,7 +94,7 @@ mod tests {
             .await
             .map_err(|e| anyhow::anyhow!("Interact error: {}", e))??;
 
-        assert_eq!(version, 19);
+        assert_eq!(version, 20);
         Ok(())
     }
 }
