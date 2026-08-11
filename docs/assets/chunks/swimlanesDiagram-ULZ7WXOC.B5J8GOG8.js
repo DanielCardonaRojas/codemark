b598f3c22d5068c2ef45a0243aa5cd369b8f7874
DanielCardonaRojas <DanielCardonaRojas@users.noreply.github.com>
2026-08-11 19:45:32 +0000

    deploy: 18e6fe9d4a6688acda8dab56e605da44d15546b6

---
+++ docs/assets/chunks/swimlanesDiagram-ULZ7WXOC.B5J8GOG8.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN.Bog-UKcn.js";import{_ as a}from"../app.CV9iwewp.js";import"./chunk-5VM5RSS4.CrLO5MfX.js";import"./chunk-XXDRQBXY.ClFTkRtf.js";import"./chunk-KBJHAD2P.P7CN6oL_.js";import"./chunk-2GRJ4B5K.DnxEomMd.js";import"./channel.BduPu25J.js";import"./framework.Bqn-bhhR.js";import"./theme.NqynPJHY.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
